6f23c76ec5d2ab162ac3a9f329e781bdc1d642ab
Claudiu Carabus <75980248+suscapusca@users.noreply.github.com>
2023-03-22 10:42:47 +0100

    AnimalPart && AnimalPartController

--- src/main/java/com/example/demo/model/AnimalPart.java
+++ src/main/java/com/example/demo/model/AnimalPart.java
@@ -1,4 +1,35 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+@Entity
 public class AnimalPart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int heads;
+    private int chests;
+    private int backs;
+    private int legs;
+
+
+    public void setHeads(int oneHead, double v) {
+    }
+
+    public void setLegs(int fourLegs, double v) {
+    }
+
+    public void setChests(int oneChest, double v) {
+    }
+
+    public void setBacks(int oneBack, double v) {
+    }
 }
