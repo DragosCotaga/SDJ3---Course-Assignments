da1ca06cbb73df10dca417942a7f79fa983f4e2e
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-03-23 16:58:53 +0100

    Continuation of the assignment

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,4 +1,22 @@
 package com.example.demo.model;
 
+import lombok.*;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
 public class Product {
+    private Long id;
+    private Map<String, Integer> parts;
+    private Double totalWeight;
+
+    public Product(Long id, Map<String, Integer> parts, Double totalWeight) {
+        this.id = id;
+        this.parts = parts;
+        this.totalWeight = totalWeight;
+    }
 }
